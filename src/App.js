96d890e0a9cf38cf51c5db2f528a8befef2071ff
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-12-03 06:24:17 +0900

    Organism作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organism/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "takiwaka",
+  image: "https://source.unsplash.com/lqqpMXO_8Tc",
+  email: "123456@gmail.com",
+  phone: "012-3456-7890",
+  company: {
+    name: "有限株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
